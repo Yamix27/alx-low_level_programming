a83a6707ede5083fe7e083fbafd3273933fab513
Yamix27 <amyne.boutallaght@gmail.com>
2023-06-23 17:50:56 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-int _putchar(char c);
+int _putchar(char);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b)
