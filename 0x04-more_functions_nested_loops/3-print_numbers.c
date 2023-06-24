5a2da51d8be65e9b641de9c7ca9d8602368e795d
Yamix27 <amyne.boutallaght@gmail.com>
2023-06-23 18:17:58 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Checks The Code.
+ *
+ * Return: This return  Nothing.
+ */
+
+void print_numbers(void)
+{
+	int nbr;
+
+	for (nbr = '0'; nbr <= '9'; nbr++)
+	{
+		_putchar (nbr);
+	}
+	_putchar ('\n');
+}
