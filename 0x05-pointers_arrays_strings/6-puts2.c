16198b369c4774bda0754b3d120007dd631f8ed0
Yamix27 <amyne.boutallaght@gmail.com>
2023-06-26 05:47:04 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,24 @@
+
 #include "main.h"
 
 /**
-* puts2 - The function prints every other character of a string
-* Followed By A New Line.
-* @str: The string from which the characters are to be printed.
+* puts2 -  func prints every other char of str
+* FOLLOWED BY ANEW LINE
+*@str: string to print the char from
 */
 void puts2(char *str)
 {
+int len = 0;
 int z;
 
-for (z = 0; str[z] != '\0'; z += 2)
+while (str[len] != '\0')
+{
+len++;
+}
+
+for (z = 0; i < len; z += 2)
 {
 _putchar(str[z]);
 }
-
 _putchar('\n');
 }
