57587ff90172524ac923a61113da5f1a37ae2718
Yamix27 <amyne.boutallaght@gmail.com>
2023-06-26 04:32:23 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap_int - sWaPping The values OF Two integers occurs.
+* @a: Pointer For first integer.
+* @b: Pointer FOR the second integer.
+*/
+void swap_int(int *a, int *b)
+{
+int z;
+
+z = *a;
+*a = *b;
+*b = z;
+}
