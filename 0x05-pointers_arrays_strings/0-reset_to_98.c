a610b477291db6f20e871cd0889f8bd2c0e954a6
Yamix27 <amyne.boutallaght@gmail.com>
2023-06-26 04:06:45 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
-
 #include "main.h"
-
 /**
- * reset_to_98 : This func TAKES a PoinTer To an int as a para
- * Also Updates The Value it points To with The value Of 98.
- * Return: 0.
- */
+* reset_to_98 : This func TAKES a PoinTer To an int as a para
+* Also Updates The Value it points To with The value Of 98.
+* Return: 0.
+*/
 void reset_to_98(int *n)
 {
     *n = 98;
