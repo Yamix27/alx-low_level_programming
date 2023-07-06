c48583ade7d1b1158676c3587deb6797eaaa3c45
Yamix27 <amyne.boutallaght@gmail.com>
2023-07-06 07:04:53 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -25,7 +25,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	return (1 + _strlen_recursion(s + 1));
+	return 1 + _strlen_recursion(s + 1);
 }
 
 /**
@@ -40,6 +40,7 @@ int check_palindrome(char *s, int start, int end)
 {
 	if (start >= end)
 		return (1);
+
 	if (s[start] != s[end])
 		return (0);
 	return check_palindrome(s, start + 1, end - 1);
