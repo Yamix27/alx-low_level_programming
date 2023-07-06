dd22777e9bc6f342d6ddfa08a6543801d05d1a38
Yamix27 <amyne.boutallaght@gmail.com>
2023-07-06 07:30:26 -0700

    #include main.h

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,15 +13,13 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*(s2 + 1) == '\0') /* '*' is at the end */
 			return (1);
-		if (*s1 && wildcmp(s1 + 1, s2)) /* move to the next character in s1 */
-			return (1);
-		if (wildcmp(s1, s2 + 1)) /* move to the next character in s2 */
+		if (*s1 == '\0' && *(s2 + 1) != '\0')
+			return (0);
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
 			return (1);
 	}
 	if (*s1 != '\0' && *s2 != '\0' && (*s1 == *s2))
-	{
-		if (wildcmp(s1 + 1, s2 + 1))
-			return (1);
-	}
+		return (wildcmp(s1 + 1, s2 + 1));
+
 	return (0);
 }
