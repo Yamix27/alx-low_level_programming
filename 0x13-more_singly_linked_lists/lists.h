8b7450440e9b7f468b3479c136954897b594bdbe
Yamix27 <amine.best196@gmail.com>
2023-07-31 09:12:05 -0700

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,14 +10,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
