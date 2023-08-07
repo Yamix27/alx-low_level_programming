76fa58d58c46f36636d601325983c34853d6b0bb
Yamix27 <amine.best196@gmail.com>
2023-08-07 08:55:06 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX stdr output.
  * @filename: The name of the file to read.
  * @letters: The number of letters it should read and print.
  *
