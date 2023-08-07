8a8e164b699c48d139661df47fb730225d7543ec
Yamix27 <amine.best196@gmail.com>
2023-08-07 09:09:57 -0700

    100-elf_header.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file and writes text content to it.
- * @filename: The name of the file to create.
- * @text_content: The content to write to the file (can be NULL).
+ * append_text_to_file - Appends text to a file.
+ * @filename: The name of the file to append to.
+ * @text_content: The content to append to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int len = 0, o, w;
 
@@ -19,8 +19,10 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
+
 	if (o == -1 || w == -1)
 		return (-1);
 
