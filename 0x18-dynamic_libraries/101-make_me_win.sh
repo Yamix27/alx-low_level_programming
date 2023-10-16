49f2f2dd8ca7dfb570c7836927f89b5f252a0172
amyne.boutallaght@gmail.com <amyne.boutallaght@gmail.com>
2023-10-16 13:05:38 -0700

    r

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -F /temp https://github.com/Yamix27/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
+wget -F /temp https://github.com/Yamix27/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/temp/libgiga.so
