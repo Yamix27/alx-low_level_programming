6e5f400416e256c04e6e824945224bfaf98338cd
Your Username <amyne.boutallagh@gmail.com>
2024-02-06 06:53:03 -0800

    test

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "search_algos.h"
-
 /**
-	* linear_search - search fr  value in an array usin the Linear search algrthm.
-	*@array: Pointer to the first element of the array.
-	*@size: Number of elements in the array.
+	*linear_search - search for value in array using Linear search algorithm.
+	*@array: Pointer to first element of array.
+	*@size: Number of elements in array.
 	*@value: Value to search for.
-	*Return: value is found, return index of first occurrence of value
-	*in array. If the value is not found or the array is NULL, return -1.
+	*Return: first occurrence of value in array, -1 not found or array is NULL.
 	*/
+
 int linear_search(int *array, size_t size, int value) /* @value: Vlue srch 4 */
 {
 	size_t i = 0;
